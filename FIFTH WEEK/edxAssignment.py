50e79b382a50ef753c96abba237c4eb4b0a791b7
James Uwuseba <James.Uwuseba@mainone.net>
2020-03-14 17:34:37 +0100

    Edex Function Question assignment

--- FIFTH WEEK/edxAssignment.py
+++ FIFTH WEEK/edxAssignment.py
@@ -1,72 +1,46 @@
 # This program is meant to add name of an animal on a list if it is elaready existing, 
 # but if it existing, already, it will be removed
-# animalList=[]
-# def list_o_matic():
-#     for p in range(20):
-#         animalNames=input("Enter the animal name here")
-#         print(animalNames)
-#         if animalNames not in animalList:
-#             animalList.append(animalNames) 
-#             print(animalList)  
-#         elif animalNames in animalList:
-#             animalList.remove(animalNames)
-#             print(animalList)
-#         elif animalNames=='':
-#             animalList.pop()
-#             print("This is the final animal list",animalList)
-#         else:
-#             print('Goodbye')
-
-# print("This is the list", a)
 
 def list_o_matic (animalList,animalNames):
     if animalNames not in animalList:
         return animalList.append(animalNames)
-    # elif animalNames in animalList:
-    #     return animalList.remove(animalNames)
-    # elif animalNames=="":
-    #     return animalList.pop()
-    # elif animalNames in animalList:
-    #     return animalList.remove(animalNames)
-    # elif animalNames not in animalList:
-    #     return animalList.append(animalNames)
+    elif animalNames in animalList:
+        return animalList.remove(animalNames)
+    elif animalNames==str():
+        return animalList.pop()
+
+animalList=[]
+animalNames=str(input('Enter the name of animal here: '))
+if ainimal !='Quit':
+    print(list_o_matic)
+else:
+    print("Goodbye")
+    
    
 animalList=['Cow','Sheep','Goat']
 animalNames=str('Bird')
 a=list_o_matic
 print(list_o_matic(['Cow','Sheep','Goat'],'Bird'))
-# if animalNames not in animalList:
-#     animalList.append(animalNames)
-#     print(animalList)
-# elif animalNames in animalList:
-#     animalList.remove(animalNames)
-#     print(animalList)
-# elif animalNames is str():
-#     animalList.pop()
-#     print(animalList)
 
 
-# animalList=[]
+if animalNames not in animalList:
+    animalList.append(animalNames)
+    print(animalList)
+elif animalNames in animalList:
+    animalList.remove(animalNames)
+    print(animalList)
+elif animalNames is str():
+    animalList.pop()
+    print(animalList)
 
-# for i in range(10):
-#     animalNames=input('Enter the name of the animal')
-#     if animalNames!="Quit":
-#         print(list_o_matic)
-#     else:
-#         print(animalList)
+
+for i in range(10):
+    animalNames=input('Enter the name of the animal')
+    if animalNames!="Quit":
+        print(list_o_matic)
+    else:
+        print(animalList)
     
 
         
-    
-
-    
-# newAddition=animalList.append("Cow")
-# print(newAddition)
-
-
-# print(b)
-# for animals in range(10):
-#     x=input("Enter the name of the animal you with check here")
-#     x=list_o_matic
-#     print(x)
 
